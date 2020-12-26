27a03eb5d5ce74e2404a51a8339b0f7f11d4883a
JOO-K <68034507+JOO-K@users.noreply.github.com>
2020-12-26 05:05:33 -0800

    v

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
             $("#home-title").css("color", "blue");
             $("#home-title").css("font-family", "MajorMono");
             $("#home-title").css("font-size", "3.5vw");
+            $("#home-title").css("letter-spacing", "-2.5px");
             $("#home-title").css("line-height", "8vh");
             $("#home-title").html("3DREAM<br>SPIRAL_RELAX")
         })
@@ -12,6 +13,7 @@ $(document).ready(function(){
         $("#home-title").css("color", "black");
         $("#home-title").css("font-family", "Helvetica");
         $("#home-title").css("font-size", "3.5vw");
+        $("#home-title").css("letter-spacing", "0px");
         $("#home-title").css("line-height", "8vh");
         $("#home-title").html("VCS '21<br>UDG SENIORS")
     })
