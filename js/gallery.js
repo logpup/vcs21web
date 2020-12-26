e59749536a307a28f5e6649b81296468d0935e76
logpup <jyjprk@gmail.com>
2020-12-26 14:50:06 -0800

    slide animation 1

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+    // Slide Animation
     $(".gallery-view-arrow-button-right").click(function(){
-      // Slide Animation
-      $(".gallery-view-img").addClass("left");
-      $(".gallery-view-img-load").addClass("load-left");
+      $(".gallery-view-img-container").addClass("left");
+    });
+
+    $(".gallery-view-arrow-button-left").click(function(){
+        $(".gallery-view-img-container").addClass("right");
+      });
+
+
+      $('.gallery-view-img-container').on('animationend webkitAnimationEnd oAnimationEnd', function () {
+        if (event.animationName == 'right') {
+            $(".gallery-view-img-container").removeClass("right");
+
+        }
+        if (event.animationName == 'left') {
+            $(".gallery-view-img-container").removeClass("left");
+        }
     });
 });
