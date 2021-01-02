be75448c47c5055f8163b450975343bdc936625c
logpup <jyjprk@gmail.com>
2021-01-01 22:59:27 -0800

    info page start

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$("document").ready(function(){
+    $(".info").click(function(){
+        $(".info-popup").removeClass("hide");
+        $(".info-popup").addClass("show");
+    })
+    $(".info-popup-arrow").click(function(){
+        $(".info-popup").addClass("hide");
+        $(".info-popup").removeClass("show");
+    })
+});
