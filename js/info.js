d2856e7cce61ad260b940e3e9997da789e9cf4bd
JOO-K <68034507+JOO-K@users.noreply.github.com>
2021-01-02 21:13:11 -0800

    commit

---
+++ js/info.js
@@ -0,0 +1,73 @@
+//Variables for setup
+
+let container;
+let camera;
+let renderer;
+let scene;
+
+function init() {
+  container = document.querySelector(".scene");
+
+  //Create scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color('#5aa6f2');
+
+  //Camera setup    
+  const fov = 35;
+  const aspect = container.clientWidth * 1.0/ container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 7, 30);
+ 
+      
+    
+  //Light setup
+  const ambient = new THREE.AmbientLight(0x404040, 2);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xffffff, .5);
+  light.position.set(50, 50, 100);
+  scene.add(light);
+    
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth * 1.0, container.clientHeight * 1.0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load Models
+  let loader = new THREE.GLTFLoader();
+    
+  loader.load("obj/info.gltf", function(gltf) {
+    scene.add(gltf.scene);
+    info = gltf.scene;
+    animate();
+  });
+          
+
+}
+
+
+function animate() {
+  
+    
+  info.rotation.y += -.01; 
+    
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+    
+}
+
+init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth * 1.0/ container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth * 1.0, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
