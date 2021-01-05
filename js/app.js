bc43c19706869e172f76784c0ef5a88cd0b03a64
logpup <jyjprk@gmail.com>
2021-01-05 17:17:07 -0500

    merge

--- js/app.js
+++ js/app.js
@@ -7,4 +7,9 @@ $("document").ready(function(){
         $(".info-popup").addClass("hide");
         $(".info-popup").removeClass("show");
     })
+    
+    let mql = window.matchMedia('(max-width: 600px)');
+    document.querySelector(".mq-value").innerText = mql.matches;
 });
+
+
