ec8bbcf882f557d4aafbd8c38d4e63a3f94e2c40
logpup <jyjprk@gmail.com>
2021-01-07 22:52:33 -0500

    student background

---
+++ js/nour.js
@@ -0,0 +1,70 @@
+//Variables for setup
+
+let container;
+let camera;
+let renderer;
+let scene;
+
+function init() {
+  container = document.querySelector(".scene-2");
+  container = document.querySelector(".scene");
+
+  //Create scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color('#ffffff');
+
+  //Camera setup    
+  const fov = 35;
+  const aspect = container.clientWidth * 1.0/ container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 0, 50);
+ 
+      
+    
+  //Light setup
+  const ambient = new THREE.AmbientLight(0x006400, 3);
+  scene.add(ambient);
+    
+  //Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth * 1.0, container.clientHeight * 1.0);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //Load Models
+  let loader = new THREE.GLTFLoader();
+    
+    loader.load("obj/cloudrainbow_prof.gltf", function(gltf) {
+    scene.add(gltf.scene);
+    cloud = gltf.scene;
+    animate();
+    });
+          
+
+}
+
+
+function animate() {
+  cloud.rotation.z += 0.004;
+  cloud.rotation.y += 0.008;
+
+    
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+    
+}
+
+init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth * 1.0/ container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth * 1.0, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
